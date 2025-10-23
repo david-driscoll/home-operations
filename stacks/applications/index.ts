83840e562bcede2d203976b602081adb6c2d7afc
David Driscoll <david.driscoll@gmail.com>
2025-10-23 15:41:15 -0400

    branding updates

--- stacks/applications/index.ts
+++ stacks/applications/index.ts
@@ -16,17 +16,21 @@ const clusterDefinition = createClusterDefinition(await op.getItemByTitle(cluste
 const outputs = new AuthentikOutputs(await op.getItemByTitle("Authentik Outputs"));
 
 // only these two are branded.
-if (clusterDefinition.key === 'sgc' || clusterDefinition.key === 'equestria') {
-  const brand = new authentik.Brand(clusterDefinition.key, {
-    domain: clusterDefinition.authentikDomain,
-    brandingLogo: clusterDefinition.icon,
-    brandingTitle: clusterDefinition.key,
-    brandingFavicon: clusterDefinition.favicon ?? "",
-    brandingDefaultFlowBackground: clusterDefinition.background ?? "/static/dist/assets/images/flow_background.jpg",
-    flowAuthentication: outputs.flows.authenticationFlow,
-    flowInvalidation: outputs.flows.providerLogoutFlow,
-    flowUserSettings: outputs.flows.userSettingsFlow,
-  });
+if (clusterDefinition.key === "sgc" || clusterDefinition.key === "equestria") {
+  const brand = new authentik.Brand(
+    clusterDefinition.key,
+    {
+      domain: clusterDefinition.authentikDomain,
+      brandingLogo: clusterDefinition.icon,
+      brandingTitle: clusterDefinition.key,
+      brandingFavicon: clusterDefinition.favicon ?? "",
+      brandingDefaultFlowBackground: clusterDefinition.background ?? "/static/dist/assets/images/flow_background.jpg",
+      flowAuthentication: outputs.flows.authenticationFlow,
+      flowInvalidation: outputs.flows.providerLogoutFlow,
+      flowUserSettings: outputs.flows.userSettingsFlow,
+    },
+    { deleteBeforeReplace: true }
+  );
 }
 
 switch (clusterDefinition.type) {
