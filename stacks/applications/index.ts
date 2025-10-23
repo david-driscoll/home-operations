30779463e5eea126b4c1d0cfebe13d16abed8821
David Driscoll <david.driscoll@gmail.com>
2025-10-23 13:38:39 -0400

    only setup branding for two clusters

--- stacks/applications/index.ts
+++ stacks/applications/index.ts
@@ -15,16 +15,19 @@ const clusterDefinition = createClusterDefinition(await op.getItemByTitle(cluste
 
 const outputs = new AuthentikOutputs(await op.getItemByTitle("Authentik Outputs"));
 
-const brand = new authentik.Brand(clusterDefinition.key, {
-  domain: clusterDefinition.authentikDomain,
-  brandingLogo: clusterDefinition.icon,
-  brandingTitle: clusterDefinition.key,
-  brandingFavicon: clusterDefinition.favicon ?? "",
-  brandingDefaultFlowBackground: clusterDefinition.background ?? "/static/dist/assets/images/flow_background.jpg",
-  flowAuthentication: outputs.flows.authenticationFlow,
-  flowInvalidation: outputs.flows.providerLogoutFlow,
-  flowUserSettings: outputs.flows.userSettingsFlow,
-});
+// only these two are branded.
+if (clusterDefinition.key === 'sgc' || clusterDefinition.key === 'equestria') {
+  const brand = new authentik.Brand(clusterDefinition.key, {
+    domain: clusterDefinition.authentikDomain,
+    brandingLogo: clusterDefinition.icon,
+    brandingTitle: clusterDefinition.key,
+    brandingFavicon: clusterDefinition.favicon ?? "",
+    brandingDefaultFlowBackground: clusterDefinition.background ?? "/static/dist/assets/images/flow_background.jpg",
+    flowAuthentication: outputs.flows.authenticationFlow,
+    flowInvalidation: outputs.flows.providerLogoutFlow,
+    flowUserSettings: outputs.flows.userSettingsFlow,
+  });
+}
 
 switch (clusterDefinition.type) {
   case "dockge":
