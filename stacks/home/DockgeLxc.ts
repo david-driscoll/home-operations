b676a36dc3226f793a6d454e36b56c385d99665b
David Driscoll <david.driscoll@gmail.com>
2025-10-25 09:59:45 -0400

    dockge does not need to accept routes?

--- stacks/home/DockgeLxc.ts
+++ stacks/home/DockgeLxc.ts
@@ -81,7 +81,7 @@ export class DockgeLxc extends ComponentResource {
       user: this.credential.apply((z) => z.fields?.username?.value!),
       password: this.credential.apply((z) => z.fields?.password?.value!),
     });
-    const tailscaleSet = installTailscale({ connection, name, parent: this, tailscaleName, globals: args.globals, args: { acceptDns: true, acceptRoutes: true, ssh: true, ...args.tailscaleArgs } });
+    const tailscaleSet = installTailscale({ connection, name, parent: this, tailscaleName, globals: args.globals, args: { acceptDns: true, acceptRoutes: false, ssh: true, ...args.tailscaleArgs } });
 
     this.dns = new StandardDns(name, { hostname: this.hostname, ipAddress, type: "A" }, args.globals, mergeOptions(cro, { dependsOn: [] }));
 
