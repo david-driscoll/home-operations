b9bdfc7391148f1d8fb5377ea2842254388630cd
David Driscoll <david.driscoll@gmail.com>
2025-10-25 01:38:15 -0400

    add some error logging

--- stacks/home/index.ts
+++ stacks/home/index.ts
@@ -20,126 +20,171 @@ const alphaSiteCluster = pulumi.output(op.getItemByTitle("Cluster: Alpha Site"))
 const equestriaCluster = pulumi.output(op.getItemByTitle("Cluster: Equestria")).apply(createClusterDefinition);
 const sgcCluster = pulumi.output(op.getItemByTitle("Cluster: Stargate Command")).apply(createClusterDefinition);
 
-const minioBucket = new minio.S3Bucket(
-  `home-operations-minio-bucket`,
-  {
-    acl: "private",
-    bucket: pulumi.interpolate`home-operations`,
-  },
-  {
-    provider: globals.truenasMinioProvider,
-    protect: true,
-    retainOnDelete: true,
-  }
-);
-
-const b2Bucket = new b2.Bucket(
-  `home-operations-b2-bucket`,
-  {
-    bucketName: pulumi.interpolate`home-operations`,
-    bucketType: "allPrivate",
-
-    bucketInfo: {
-      project: "home-operations",
-      purpose: "pulumi storage",
+try {
+  const minioBucket = new minio.S3Bucket(
+    `home-operations-minio-bucket`,
+    {
+      acl: "private",
+      bucket: pulumi.interpolate`home-operations`,
     },
-    lifecycleRules: [
-      {
-        fileNamePrefix: "",
-        daysFromHidingToDeleting: 1,
+    {
+      provider: globals.truenasMinioProvider,
+      protect: true,
+      retainOnDelete: true,
+    }
+  );
+
+  const b2Bucket = new b2.Bucket(
+    `home-operations-b2-bucket`,
+    {
+      bucketName: pulumi.interpolate`home-operations`,
+      bucketType: "allPrivate",
+
+      bucketInfo: {
+        project: "home-operations",
+        purpose: "pulumi storage",
       },
-    ],
-  },
-  {
-    provider: globals.backblazeProvider,
-    protect: true,
-    retainOnDelete: true,
-  }
-);
+      lifecycleRules: [
+        {
+          fileNamePrefix: "",
+          daysFromHidingToDeleting: 1,
+        },
+      ],
+    },
+    {
+      provider: globals.backblazeProvider,
+      protect: true,
+      retainOnDelete: true,
+    }
+  );
+} catch (e) {
+  console.error("Error creating buckets:", e);
+  throw e;
+}
 
-var twilightSparkleHost = new ProxmoxHost("twilight-sparkle", {
-  title: "Twilight Sparkle",
-  globals: globals,
-  isProxmoxBackupServer: false,
-  internalIpAddress: "10.10.10.100",
-  tailscaleIpAddress: "100.111.10.100",
-  macAddress: "58:47:ca:7b:a9:9d",
-  proxmox: mainProxmoxCredentials,
-  remote: false,
-  cluster: celestiaCluster,
-});
+try {
+  var twilightSparkleHost = new ProxmoxHost("twilight-sparkle", {
+    title: "Twilight Sparkle",
+    globals: globals,
+    isProxmoxBackupServer: false,
+    internalIpAddress: "10.10.10.100",
+    tailscaleIpAddress: "100.111.10.100",
+    macAddress: "58:47:ca:7b:a9:9d",
+    proxmox: mainProxmoxCredentials,
+    remote: false,
+    cluster: celestiaCluster,
+  });
+} catch (e) {
+  console.error("Error creating twilight sparkle host:", e);
+  throw e;
+}
 
-const spikeVm = new TruenasVm("spike", {
-  credential: globals.truenasCredential.apply((z) => z.title!),
-  globals: globals,
-  host: twilightSparkleHost,
-  ipAddress: pulumi.output("10.10.10.10"),
-  tailscaleIpAddress: "100.111.10.10",
-  macAddress: "bc:24:11:7c:e5:c5",
-});
+try {
+  var spikeVm = new TruenasVm("spike", {
+    credential: globals.truenasCredential.apply((z) => z.title!),
+    globals: globals,
+    host: twilightSparkleHost,
+    ipAddress: pulumi.output("10.10.10.10"),
+    tailscaleIpAddress: "100.111.10.10",
+    macAddress: "bc:24:11:7c:e5:c5",
+  });
+} catch (e) {
+  console.error("Error creating spike VM:", e);
+  throw e;
+}
 
-var celestiaHost = new ProxmoxHost("celestia", {
-  globals: globals,
-  isProxmoxBackupServer: true,
-  internalIpAddress: "10.10.10.103",
-  tailscaleIpAddress: "100.111.10.103",
-  macAddress: "c8:ff:bf:03:cc:4c",
-  proxmox: mainProxmoxCredentials,
-  truenas: spikeVm,
-  remote: false,
-  cluster: celestiaCluster,
-});
+try {
+  var celestiaHost = new ProxmoxHost("celestia", {
+    globals: globals,
+    isProxmoxBackupServer: true,
+    internalIpAddress: "10.10.10.103",
+    tailscaleIpAddress: "100.111.10.103",
+    macAddress: "c8:ff:bf:03:cc:4c",
+    proxmox: mainProxmoxCredentials,
+    truenas: spikeVm,
+    remote: false,
+    cluster: celestiaCluster,
+  });
+} catch (e) {
+  console.error("Error creating celestia host:", e);
+  throw e;
+}
 
-var lunaHost = new ProxmoxHost("luna", {
-  globals: globals,
-  isProxmoxBackupServer: true,
-  tailscaleIpAddress: "100.111.10.104",
-  macAddress: "c8:ff:bf:03:c9:1e",
-  proxmox: mainProxmoxCredentials,
-  truenas: spikeVm,
-  remote: true,
-  cluster: lunaCluster,
-});
+try {
+  var lunaHost = new ProxmoxHost("luna", {
+    globals: globals,
+    isProxmoxBackupServer: true,
+    tailscaleIpAddress: "100.111.10.104",
+    macAddress: "c8:ff:bf:03:c9:1e",
+    proxmox: mainProxmoxCredentials,
+    truenas: spikeVm,
+    remote: true,
+    cluster: lunaCluster,
+  });
+} catch (e) {
+  console.error("Error creating luna host:", e);
+  throw e;
+}
 
-var alphaSiteHost = new ProxmoxHost("alpha-site", {
-  globals: globals,
-  isProxmoxBackupServer: false,
-  internalIpAddress: "10.10.10.200",
-  tailscaleIpAddress: "100.111.10.200",
-  macAddress: "e4:5f:01:90:36:22",
-  proxmox: alphaSiteProxmoxCredentials,
-  installTailscale: false,
-  remote: false,
-  cluster: alphaSiteCluster,
-  shortName: "as",
-});
+try {
+  var alphaSiteHost = new ProxmoxHost("alpha-site", {
+    globals: globals,
+    isProxmoxBackupServer: false,
+    internalIpAddress: "10.10.10.200",
+    tailscaleIpAddress: "100.111.10.200",
+    macAddress: "e4:5f:01:90:36:22",
+    proxmox: alphaSiteProxmoxCredentials,
+    installTailscale: false,
+    remote: false,
+    cluster: alphaSiteCluster,
+    shortName: "as",
+  });
+} catch (e) {
+  console.error("Error creating alpha site host:", e);
+  throw e;
+}
 
-var celestiaDockgeRuntime = new DockgeLxc("celestia-dockge", {
-  globals,
-  credential: dockgeCredential,
-  host: celestiaHost,
-  vmId: 300,
-  cluster: celestiaCluster,
-});
+try {
+  var celestiaDockgeRuntime = new DockgeLxc("celestia-dockge", {
+    globals,
+    credential: dockgeCredential,
+    host: celestiaHost,
+    vmId: 300,
+    cluster: celestiaCluster,
+  });
+} catch (e) {
+  console.error("Error creating celestia VM:", e);
+  throw e;
+}
 
-var lunaDockgeRuntime = new DockgeLxc("luna-dockge", {
-  globals,
-  credential: dockgeCredential,
-  host: lunaHost,
-  vmId: 400,
-  cluster: lunaCluster,
-});
+try {
+  var lunaDockgeRuntime = new DockgeLxc("luna-dockge", {
+    globals,
+    credential: dockgeCredential,
+    host: lunaHost,
+    vmId: 400,
+    cluster: lunaCluster,
+  });
+} catch (e) {
+  console.error("Error creating luna VM:", e);
+  throw e;
+}
 
-var alphaSiteDockgeRuntime = new DockgeLxc("alpha-site-dockge", {
-  globals,
-  credential: dockgeCredential,
-  host: alphaSiteHost,
-  vmId: 100,
-  ipAddress: "10.10.10.9",
-  tailscaleIpAddress: "100.111.10.9",
-  cluster: alphaSiteCluster,
-  tailscaleArgs: { acceptRoutes: false },
-});
+try {
+  var alphaSiteDockgeRuntime = new DockgeLxc("alpha-site-dockge", {
+    globals,
+    credential: dockgeCredential,
+    host: alphaSiteHost,
+    vmId: 100,
+    ipAddress: "10.10.10.9",
+    tailscaleIpAddress: "100.111.10.9",
+    cluster: alphaSiteCluster,
+    tailscaleArgs: { acceptRoutes: false },
+  });
+} catch (e) {
+  console.error("Error creating alpha site dockge runtime:", e);
+  throw e;
+}
 
 // TODO: add code to ensure tailscale ips is set for all important services
 
