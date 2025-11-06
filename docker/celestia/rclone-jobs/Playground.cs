b9103e4129c9d0cda3587c85efa7e104138b69ec
David Driscoll <david.driscoll@gmail.com>
2025-11-06 00:11:00 -0500

    manager start

--- docker/celestia/rclone-jobs/Playground.cs
+++ docker/celestia/rclone-jobs/Playground.cs
@@ -87,6 +87,8 @@ JobManager.AddJob(async () =>
     await RunJobs();
 }, a => a.ToRunEvery(5).Days().At(10, 00));
 
+JobManager.Start();
+
 static async Task DownloadRclone()
 {
     var rcloneItem = Path.Combine(Path.GetTempPath(), "rclone.zip");
@@ -152,7 +154,6 @@ record SftpBackend(string Remote, string Host) : RCloneBackend(Remote)
         yield return new KeyValuePair<string, string>($"RCLONE_CONFIG_{Remote.ToUpper()}_PORT", "2022");
         yield return new KeyValuePair<string, string>($"RCLONE_CONFIG_{Remote.ToUpper()}_USER", "sftp");
         yield return new KeyValuePair<string, string>($"RCLONE_CONFIG_{Remote.ToUpper()}_KEY_FILE", "/keys/id_ed25519");
-        // yield return new KeyValuePair<string, string>($"RCLONE_CONFIG_{Remote.ToUpper()}_KNOWN_HOSTS_FILE", "/keys/known_hosts");
     }
     public override string GetRemotePath(string path) => $"{Remote}:{path}";
 }
