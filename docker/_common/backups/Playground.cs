7818bb63739550f7300923153b31e88dcdeed31b
David Driscoll <david.driscoll@gmail.com>
2025-11-06 21:13:29 -0500

    moving backup to shared stack, with new behavior

--- docker/_common/backups/Playground.cs
+++ docker/_common/backups/Playground.cs
@@ -3,16 +3,19 @@
 #:package CliWrap@3.9.0
 #:package Dumpify@0.6.6
 #:package 1Password.Connect.Sdk@1.0.4
+#:package Blobject.NFS@5.0.14
+
 
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 using System.IO.Compression;
+using Blobject.NFS;
 using CliWrap;
 using CliWrap.Buffered;
 using Dumpify;
 using FluentScheduler;
 using OnePassword.Connect.Sdk;
-using Renci.SshNet.Security;
+using OnePassword.Connect.Sdk.Models;
 
 var httpClient = new HttpClient();
 
@@ -25,64 +28,69 @@ var vault = ( await client.GetVaultsAsync($"name eq \"Eris\"") ).Single();
 // var lunaDockge = await GetItemByTitle(client, vault.Id, "DockgeLxc: Luna");
 // var alphaSiteDockge = await GetItemByTitle(client, vault.Id, "DockgeLxc: Alpha Site");
 
-static async Task RunJobs()
+var jobs = Directory.EnumerateFiles("/jobs", "*.json")
+.Select(path =>
 {
-    var sgcVolsyncFolders =
-Directory.EnumerateDirectories("/spike/backup/sgc/volsync")
-.Select(path => (key: "sgc", path));
-    var equestriaVolsyncFolders =
-Directory.EnumerateDirectories("/spike/backup/equestria/volsync")
-.Select(path => (key: "equestria", path));
-    var localBackend = new LocalBackend("local");
-    var lunaSftpBackend = new SftpBackend("luna", "dockge-luna.${tailscaleDomain}");
+    var content = System.IO.File.ReadAllText(path);
+    return System.Text.Json.JsonSerializer.Deserialize<BackupTask>(content)!;
+})
+.ToArray();
 
-    foreach (var item in sgcVolsyncFolders.Concat(equestriaVolsyncFolders))
+var rcloneJobs = await Task.WhenAll(jobs.Select(async job =>
+{
+    var sourceBackend = await CreateBackend("source", job.SourceType, job.SourcePath, job.SourceSecret);
+    var destinationBackend = await CreateBackend("destination", job.DestinationType, job.DestinationPath, job.DestinationSecret);
+
+    return new RCloneJob(
+        job.Name,
+        sourceBackend,
+        job.SourcePath,
+        destinationBackend,
+        job.DestinationPath
+    );
+}));
+
+async Task<RCloneBackend> CreateBackend(string name, string type, string path, string? secret)
+{
+    FullItem? secretItem = null;
+    if (secret is { Length: > 0 })
     {
-        try
-        {
-            Console.WriteLine($"Starting sync for {item.key}-{Path.GetFileName(item.path)} to local");
-            await Rclone(
-                $"{item.key}-{Path.GetFileName(item.path)}",
-                localBackend,
-                item.path,
-                localBackend,
-                $"/data/backup/{item.key}/volsync/{Path.GetFileName(item.path)}"
-            );
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error syncing {item.key}-{Path.GetFileName(item.path)}: {ex.Message}");
-        }
+        secretItem = await GetItemByTitle(client, vault.Id, secret);
     }
+    return type switch
+    {
+        "local" => new LocalBackend(name),
+        "sftp" => new SftpBackend(name, path),
+        "b2" => new B2Backend(name, secretItem!.GetField("bucket").Value!, secretItem!.GetField("username").Value!, secretItem!.GetField("credential").Value!),
+        // "s3" => new S3Backend(name, ),
+        _ => throw new InvalidOperationException($"Unknown backend type: {type}"),
+    };
+}
 
-    foreach (var item in sgcVolsyncFolders.Concat(equestriaVolsyncFolders))
+static async Task RunJobs(RCloneJob[] jobs)
+{
+    foreach (var job in jobs)
     {
         try
         {
-            Console.WriteLine($"Starting sync for {item.key}-{Path.GetFileName(item.path)} to luna");
-            await Rclone(
-                $"{item.key}-{Path.GetFileName(item.path)}",
-                localBackend,
-                $"/data/backup/{item.key}/volsync/{Path.GetFileName(item.path)}",
-                lunaSftpBackend,
-                // destination here is already mapped by sftp
-                $"/{item.key}/volsync/{Path.GetFileName(item.path)}"
-            );
+            Console.WriteLine($"Starting job {job.Name}");
+            await Rclone(job);
         }
         catch (Exception ex)
         {
-            Console.WriteLine($"Error syncing {item.key}-{Path.GetFileName(item.path)}: {ex.Message}");
+            Console.WriteLine($"Error running job {job.Name}: {ex.Message}");
         }
     }
 }
 
+await DownloadRclone();
+await RunJobs(rcloneJobs);
 JobManager.Initialize();
 
 JobManager.AddJob(async () =>
 {
     Console.WriteLine("Starting Rclone Jobs");
-    await DownloadRclone();
-    await RunJobs();
+    await RunJobs(rcloneJobs);
     Console.WriteLine("Rclone Jobs Completed");
 }, a => a.ToRunEvery(5).Days().At(10, 00));
 
@@ -95,7 +103,7 @@ static async Task DownloadRclone()
     {
         var client = new HttpClient();
         using var rcloneStream = await client.GetStreamAsync("https://downloads.rclone.org/rclone-current-linux-amd64.zip");
-        using var writeStream = File.OpenWrite(rcloneItem);
+        using var writeStream = System.IO.File.OpenWrite(rcloneItem);
         await rcloneStream.CopyToAsync(writeStream);
         await rcloneStream.FlushAsync();
         await writeStream.FlushAsync();
@@ -109,34 +117,55 @@ static async Task DownloadRclone()
         .ExecuteAsync();
 }
 
-static async Task Rclone(string name, RCloneBackend source, string sourcePath, RCloneBackend destination, string destinationPath)
+static async Task Rclone(RCloneJob job)
 {
+    using var httpClient = new HttpClient();
     var item = await Cli.Wrap(Path.Combine(Path.GetTempPath(), "rclone"))
     .WithStandardOutputPipe(PipeTarget.ToStream(Console.OpenStandardOutput()))
     .WithStandardErrorPipe(PipeTarget.ToStream(Console.OpenStandardError()))
     .WithEnvironmentVariables(
-        source.GetEnvironmentVariables()
-        .Concat(destination.GetEnvironmentVariables())
+        job.Source.GetEnvironmentVariables()
+        .Concat(job.Destination.GetEnvironmentVariables())
         .DistinctBy(kv => kv.Key)
         .ToDictionary(kv => kv.Key, kv => (string?)kv.Value)
     )
         .WithArguments(args =>
         args
         .Add("sync")
-        .Add(source.GetRemotePath(sourcePath))
-        .Add(destination.GetRemotePath(destinationPath))
+        .Add(job.Source.GetRemotePath(job.SourcePath))
+        .Add(job.Destination.GetRemotePath(job.DestinationPath))
         .Add("--progress")
     )
         .ExecuteAsync();
 
 
-    Console.WriteLine($"{name} exited with code {item.ExitCode} in {item.RunTime.ToString("c")}");
+    if (item.ExitCode != 0)
+    {
+        Console.WriteLine($"Rclone job {job.Name} failed with exit code {item.ExitCode}");
+    }
+    try
+    {
+        var request = new HttpRequestMessage(HttpMethod.Post, $"$UPTIME_API_URL/api/v1/endpoints/{job.Name}/external?success={( item.ExitCode == 0 ).ToString().ToLower()}&error={( item.ExitCode == 0 ? "" : $"Rclone job {job.Name} failed with exit code {item.ExitCode}" )}&duration={item.RunTime:c}")
+        {
+            Headers = { Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", job.Name) }
+        };
+        var response = await httpClient.SendAsync(request);
+        response.Dump();
+        await response.Content.ReadAsStringAsync().Dump();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error reporting to Uptime API: {ex.Message}");
+        ex.Dump();
+    }
+    Console.WriteLine($"{job.Name} exited with code {item.ExitCode} in {item.RunTime:c}");
 }
 
-static async Task<OnePassword.Connect.Sdk.Models.Item> GetItemByTitle(OnePasswordConnectClient client, string vaultId, string title)
+static async Task<FullItem> GetItemByTitle(OnePasswordConnectClient client, string vaultId, string title)
 {
     var items = await client.GetVaultItemsAsync(vaultId, $"title eq \"{title}\"");
-    return items.Single();
+    var item = await client.GetVaultItemByIdAsync(vaultId, items.Single().Id);
+    return item;
 }
 
 abstract record RCloneBackend(string Remote)
@@ -192,4 +221,16 @@ record LocalBackend(string Remote) : RCloneBackend(Remote)
     }
 
     public override string GetRemotePath(string path) => path;
+}
+
+record BackupTask(string Name, string SourceType, string SourcePath, string? SourceSecret, string DestinationType, string DestinationPath, string? DestinationSecret);
+record RCloneJob(string Name, RCloneBackend Source, string SourcePath, RCloneBackend Destination, string DestinationPath);
+
+static class RCloneBackendExtensions
+{
+    public static Field GetField(this FullItem item, string key)
+    {
+        return item.Fields.First(z => z.Label == key);
+    }
+
 }
