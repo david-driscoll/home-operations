87acc610d101666f86193f49b85ca66a785158e1
David Driscoll <david.driscoll@gmail.com>
2025-11-16 13:34:18 -0500

    updated home-operations

--- components/authentik.ts
+++ components/authentik.ts
@@ -421,6 +421,7 @@ export class AuthentikApplicationManager extends pulumi.ComponentResource {
     for (let i = 0; i < gatusDefinitions.length; i++) {
       const endpoint = gatusDefinitions[i];
       endpoint.name = `${definition.spec.name} ${endpoint.name ?? (i == 0 ? "" : i + 1).toString()}`;
+      endpoint.group ??= definition.spec.category;
       endpoint.interval ??= "1m";
       endpoint.alerts ??= [];
       endpoint.alerts.push({
