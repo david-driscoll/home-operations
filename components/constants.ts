eabcffc832037d3105dca1661dd8bd9cfd1d147e
David Driscoll <david.driscoll@gmail.com>
2026-06-16 23:30:03 -0400

    updated constants

--- components/constants.ts
+++ components/constants.ts
@@ -68,7 +68,7 @@ export const Tailscale = {
     ssh: ["tcp:22", "udp:22"] as TailscaleNetworkCapability[],
     dockge: ["tcp:80", "tcp:443"] as TailscaleNetworkCapability[],
     nfs: ["tcp:2049", "udp:2049", "tcp:111", "udp:111"] as TailscaleNetworkCapability[],
-    dockgeManagement: ["tcp:5001", "udp:5001", "tcp:2022", "udp:2022", "tcp:2375", "udp:2375", "tcp:8082", "tcp:4000"] as TailscaleNetworkCapability[],
+    dockgeManagement: ["tcp:5001", "udp:5001", "tcp:2022", "udp:2022", "tcp:2375", "udp:2375", "tcp:8082", "tcp:4000", "tcp:53443"] as TailscaleNetworkCapability[],
     observability: [
       "tcp:9093",
       "tcp:19291",
