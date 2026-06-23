8a0d645f42d4c39425bc37a3b0c835741352bca6
David Driscoll <david.driscoll@gmail.com>
2026-06-23 14:26:17 -0400

    updated unifi

--- stacks/unifi-network/acl-manager.ts
+++ stacks/unifi-network/acl-manager.ts
@@ -398,13 +398,13 @@ export function assignTailscaleAcls(globals: GlobalResources): pulumi.Output<any
           },
           {
             address: secondaryDnsIp,
-            useWithExitNode: true,
+            useWithExitNode: FileSystemWritableFileStream,
           },
           // {
           //   address: unifiDnsIp,
           //   useWithExitNode: false,
           // },
-          // ...dns.internalIps.map((ip) => ({ address: ip, useWithExitNode: false })),
+          ...dns.internalIps.map((ip) => ({ address: ip, useWithExitNode: false })),
         ],
       },
       cro,
