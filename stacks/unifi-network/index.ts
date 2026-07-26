2389394ca8e835567f63d86e8dec69a02a9b9aaf
David Driscoll <david.driscoll@gmail.com>
2026-07-26 14:36:18 -0400

    updated stacks and disable firewall rules for now

--- stacks/unifi-network/index.ts
+++ stacks/unifi-network/index.ts
@@ -2,6 +2,6 @@ import { GlobalResources } from "../../components/globals.ts";
 import { assignTailscaleAcls } from "./acl-manager.ts";
 import { createTailscaleAttDropFirewallRule } from "./tailscale-drop-firewall-rule.ts";
 
-const globals = new GlobalResources("global-resources", {});
-createTailscaleAttDropFirewallRule(globals);
+const globals = new GlobalResources({}, {});
+// createTailscaleAttDropFirewallRule(globals);
 assignTailscaleAcls(globals);
